36500df12466a1e19a193ae1edb42e60c36b6730
francoda <francodealesandro@gmail.com>
2017-09-26 18:54:05 -0300

    Documentacion

---
+++ Documentacion/config.py
@@ -0,0 +1,13 @@
+from twitter import *
+
+WORLD_WOE_ID = 1
+ARGENTINA_WOE_ID = 23424747
+
+#Completar los tokens correspondientes
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+ACCESS_TOKEN_KEY = ""
+ACCESS_TOKEN_SECRET = ""
+
+
+
