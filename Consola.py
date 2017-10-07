b9915a22b43f36d48ab26b5e75506816c025ff29
francoda <francodealesandro@gmail.com>
2017-10-06 23:30:54 -0300

    Fix al ejecutar consola por cmd

--- Consola.py
+++ Consola.py
@@ -15,23 +15,26 @@ class Menu():
         self.s.run()
 
     def Ciclo(self, sc):
-        #Pido a la API los tweets
-        resultados = self.tw.search.tweets(
-            q=str(' OR ').join([e.value for e in Candidato]),
-            result_type='recent',
-            count=100)
-        for tweet in resultados['statuses']:
-            print(tweet['text'])
-            for candidato in [e.value for e in Candidato]:
-                if candidato in tweet['text']:
-                    try:
-                        self.dicc[candidato][tweet['id']] = tweet
-                    except KeyError:
-                        self.dicc[candidato] = {tweet['id']: tweet}
-        Persistencia.guardar(self.dicc)
-        #Imprimo resultados
-        self.resumen()
-        self.s.enter(20, 1, self.Ciclo, (sc,))
+        try:
+            #Pido a la API los tweets
+            resultados = self.tw.search.tweets(
+                q=str(' OR ').join([e.value for e in Candidato]),
+                result_type='recent',
+                count=100)
+            for tweet in resultados['statuses']:
+                print(ascii(tweet['text']))
+                for candidato in [e.value for e in Candidato]:
+                    if candidato in tweet['text']:
+                        try:
+                            self.dicc[candidato][tweet['id']] = tweet
+                        except KeyError:
+                            self.dicc[candidato] = {tweet['id']: tweet}
+            Persistencia.guardar(self.dicc)
+            #Imprimo resultados
+            self.resumen()
+            self.s.enter(20, 1, self.Ciclo, (sc,))
+        except Exception as ex:
+            lala = input(str(ex))
 
     def limpiar(self):
         os.system('cls' if os.name=='nt' else 'clear')
