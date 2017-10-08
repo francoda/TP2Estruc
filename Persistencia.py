de1a2a23616d079b665374a93179c178e767bd20
francoda <francodealesandro@gmail.com>
2017-10-08 17:15:25 -0300

    Limpieza de Interface y continuar al encontrar error

--- Persistencia.py
+++ Persistencia.py
@@ -3,14 +3,16 @@ from Consola import Candidato
 import os
 
 def cargar():
-    dicc = {}
+    dicc = {e.value:{} for e in Candidato}
     for candidato in [e.value for e in Candidato]:
-        dicc[candidato] = {}
-        file = open(os.getcwd() + '\\' + str(candidato).replace('@', '').lower() + '.j', 'r')
-        lista = json.load(file)
-        for tweet in lista:
-            dicc[candidato][tweet['id']] = tweet
-        file.close()
+        try:
+            file = open(os.getcwd() + '\\' + str(candidato).replace('@', '').lower() + '.j', 'r')
+            lista = json.load(file)
+            for tweet in lista:
+                dicc[candidato][tweet['id']] = tweet
+            file.close()
+        except FileNotFoundError:
+            dicc[candidato] = {}
     return dicc
 
 def guardar(dicc):
@@ -21,3 +23,23 @@ def guardar(dicc):
             lista.append(tweet)
         json.dump(lista, file)
         file.close()
+
+def cargarEstadisticas():
+    try:
+        last_id = 0
+        fist_id = 0
+        file = open(os.getcwd() + '\\Estadisticas.txt', 'r')
+        for line in file.readlines():
+            if 'last_id' in line:
+                last_id = int(line.split(":",1)[1])
+            elif 'fist_id' in line:
+                fist_id = int(line.split(":",1)[1])
+        file.close()
+        return last_id, fist_id
+    except FileNotFoundError:
+        return 0, 0
+
+def guardarEstadisticas(resumen):
+    file = open(os.getcwd() + '\\Estadisticas.txt', 'w')
+    file.writelines(resumen)
+    file.close()
