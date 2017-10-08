ffa58175d16df01065618d4cac1823607899e7ff
francoda <francodealesandro@gmail.com>
2017-10-08 16:46:03 -0300

    Nueva Interface y +Estadisticas

--- Consola.py
+++ Consola.py
@@ -9,42 +9,61 @@ class Menu():
     def __init__(self):
         self.tw = Twitter(auth=OAuth(ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET))
         self.dicc = Persistencia.cargar()
-        self.resumen()
+        self.last_id, self.fist_id = Persistencia.cargarEstadisticas()
+        self.resumen({e.value:0 for e in Candidato})
         self.s = sched.scheduler(time.time, time.sleep)
-        self.s.enter(20, 1, self.Ciclo, (self.s,))
+        self.s.enter(15, 1, self.Ciclo, (self.s,))
         self.s.run()
 
     def Ciclo(self, sc):
         try:
+            diccContador = {e.value:0 for e in Candidato}
+            tweets = []
             #Pido a la API los tweets
-            resultados = self.tw.search.tweets(
-                q=str(' OR ').join([e.value for e in Candidato]),
-                result_type='recent',
-                count=100)
+            if self.last_id == 0:
+                resultados = self.tw.search.tweets(
+                    q=str(' OR ').join([e.value for e in Candidato]),
+                    result_type='recent',
+                    count=100)
+            else:
+                resultados = self.tw.search.tweets(
+                    q=str(' OR ').join([e.value for e in Candidato]),
+                    result_type='recent',
+                    count=100,
+                    since_id=self.last_id)
+            if len(resultados['statuses']) > 0:
+                self.last_id = resultados['statuses'][0]['id']
             for tweet in resultados['statuses']:
                 for candidato in [e.value for e in Candidato]:
                     if candidato in tweet['text']:
+                        tweets.append(ascii(tweet['text']))
+                        diccContador[candidato] += 1
                         try:
                             self.dicc[candidato][tweet['id']] = tweet
                         except KeyError:
                             self.dicc[candidato] = {tweet['id']: tweet}
             #Imprimo resultados
             self.limpiar()
-            self.resumen()
+            self.resumen(diccContador)
+            for tweet in tweets:
+                print(tweet)
         except Exception as ex:
-            Persistencia.guardar(self.dicc)
             input(str(ex) + '\n Preciona Enter para continuar...')
-        self.s.enter(20, 1, self.Ciclo, (sc,))
+        Persistencia.guardar(self.dicc)
+        print('Puede Cerrar')
+        self.s.enter(15, 1, self.Ciclo, (sc,))
 
     def limpiar(self):
         os.system('cls' if os.name=='nt' else 'clear')
 
-    def resumen(self):
-        contador = 0
+    def resumen(self, diccContador):
+        resumen = ''
         for candidato, tweets in self.dicc.items():
-            print(candidato, ': ', len(tweets))
-            contador += len(tweets)
-        print('Total: ', contador)
+            resumen += candidato + ': ' + str(len(tweets)) + ' (+' + str(diccContador[candidato]) + ')\n'
+        resumen += 'Total: ' + str(sum([len(x) for x in self.dicc.values()])) + ' (+' + str(sum(diccContador.values())) + ')\n'
+        print(resumen)
+        resumen += 'last_id:' + str(self.last_id) + '\nfist_id:' + str(self.fist_id)
+        Persistencia.guardarEstadisticas(resumen)
 
 class Candidato(Enum):
     CFKARGENTINA = '@CFKArgentina'
